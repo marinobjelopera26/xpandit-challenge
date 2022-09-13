6911ee3c035a4cb621c6db21dbb429e052f6bf31
Marino Bjelopera <marino.bjelopera26@gmail.com>
2022-09-13 21:04:22 +0200

    #5: create DB data seeders

---
+++ src/XpandIT.Challenge.DataLayer/Seeders/Initializers/ContactPhoneNumberInitializer.cs
@@ -0,0 +1,34 @@
+﻿using XpandIT.Challenge.DataLayer.Entities;
+
+namespace XpandIT.Challenge.DataLayer.Seeders.Initializers
+{
+    internal class ContactPhoneNumberInitializer : DbInitializerBase<ContactPhoneNumberInitializer>
+    {
+        private readonly ISet<int> _usedPhoneNumberIds = new HashSet<int>();
+
+        public override void Execute()
+        {
+            EnsureDbContextExists();
+
+            foreach (Contact contact in DbContext!.Contacts!.ToList())
+            {
+                contact.PhoneNumbers ??= new List<ContactPhoneNumber>();
+
+                int? phoneNumberId = DbContext.PhoneNumbers?.FirstOrDefault(x => !_usedPhoneNumberIds.Contains(x.Id))?.Id;
+
+                if (phoneNumberId.HasValue) {
+                    contact.PhoneNumbers.Add(
+                        new ContactPhoneNumber()
+                        {
+                            ContactId = contact.Id,
+                            PhoneNumberId = phoneNumberId.Value
+                        });
+
+                    _usedPhoneNumberIds.Add(phoneNumberId.Value);
+                }
+            }
+
+            DbContext.SaveChanges();
+        }
+    }
+}
---
+++ src/XpandIT.Challenge.DataLayer/Seeders/Initializers/PhoneNumberInitializer.cs
@@ -0,0 +1,31 @@
+﻿using XpandIT.Challenge.DataLayer.Entities;
+
+namespace XpandIT.Challenge.DataLayer.Seeders.Initializers
+{
+    internal class PhoneNumberInitializer : DbInitializerBase<PhoneNumberInitializer>
+    {
+        public override void Execute()
+        {
+            EnsureDbContextExists();
+
+            if (DbContext!.PhoneNumbers?.Any() == true)
+                return;
+
+            List<PhoneNumber> phoneNumbers = new();
+
+            // Generate 80 random phone numbers
+            for (int i = 0; i < 80; i++)
+            {
+                phoneNumbers.Add(
+                    new PhoneNumber
+                    {
+                        Type = Faker!.Random.Number(min: 1, max: 6),
+                        Value = Faker.Phone.PhoneNumber()
+                    });
+            }
+            
+            DbContext.PhoneNumbers?.AddRange(phoneNumbers);
+            DbContext.SaveChanges();
+        }
+    }
+}
